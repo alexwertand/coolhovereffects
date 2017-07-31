a3158a3707ebd2f5a5d4a7f984afdfe7f0930078
Alex And <alexandr_works@ukr.net>
2017-07-31 10:53:36 +0300

    ready task in sample.js

--- sample.js
+++ sample.js
@@ -1,1 +1,8 @@
-console.log('hello');
+function consoleRec (source, number) {
+    console.log(source[number++]);
+    if (number < source.length) consoleRec(source, number);
+}
+
+var arr = ['я', 'умею', 'писать', 'рекурсивные', 'функции'];
+
+consoleRec(arr, 0);
